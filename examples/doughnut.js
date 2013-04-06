ec4b3b599ad33c17ba2e1b47aa917fec515f55cd
cfddream <cfddream@gmail.com>
2013-04-06 12:39:15 +0800

    added `doughnut` chart

---
+++ examples/doughnut.js
@@ -0,0 +1,13 @@
+var Canvas = require('canvas'),
+    canvas = new Canvas(800, 800),
+    ctx = canvas.getContext('2d'),
+    Chart = require('../'),
+    fs = require('fs'),
+    data = JSON.parse(fs.readFileSync('./doughnut.json'));
+
+Chart(ctx).Doughnut(data);
+
+canvas.toBuffer(function (err, buf) {
+  if (err) throw err;
+  fs.writeFile(__dirname + '/doughnut.png', buf);
+});

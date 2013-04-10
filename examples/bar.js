ccdd9daf0f77f9a6a8e0b81a10cb291bc3153633
cfddream <cfddream@gmail.com>
2013-04-10 23:09:59 +0800

    added `Bar` Chart

---
+++ examples/bar.js
@@ -0,0 +1,16 @@
+var Canvas = require('canvas'),
+    canvas = new Canvas(600, 400),
+    ctx = canvas.getContext('2d'),
+    Chart = require('../'),
+    fs = require('fs'),
+    data = JSON.parse(fs.readFileSync('./bar.json'));
+
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+Chart(ctx).Bar(data);
+
+canvas.toBuffer(function (err, buf) {
+  if (err) throw err;
+  fs.writeFile(__dirname + '/bar.png', buf);
+});
+
